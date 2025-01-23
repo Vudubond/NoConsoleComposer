c551b203a9e874b6e2b9b3230746ef87460a50a6
Vu3u <vudubond@gmail.com>
2025-01-23 17:36:34 +0200

    Update password.php

--- composer/password.php
+++ composer/password.php
@@ -1,8 +1,16 @@
 <?php
-$password = "password1234";
-if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_PW'] !== $password)
-{
+
+if (isset($_SERVER['HTTP_AUTHORIZATION']) && strpos($_SERVER['HTTP_AUTHORIZATION'], 'Basic ') === 0) {
+    // Decode Base64-encoded "username:password"
+    $auth = base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6));
+    list($user, $pass) = explode(':', $auth, 2);
+} else {
+    $user = $pass = null;
+}
+
+$password = "fuckinghardpassword1985!";
+if ($pass !== $password) {
     header('WWW-Authenticate: Basic realm="NoConsoleComposer"');
     header('HTTP/1.0 401 Unauthorized');
-    exit;
+    exit('Unauthorized');
 }
